2490baa3fb4aa9608884d37f3de11a5347085a99
Vladimir <boonoovovan@yandex.ru>
2019-05-01 00:32:18 +1000

    init

---
+++ SOM.h
@@ -0,0 +1,19 @@
+//
+//  SOM.h
+//  SOM
+//
+//  Created by Vladimir on 01/05/2019.
+//  Copyright © 2019 Vladimir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SOM.
+FOUNDATION_EXPORT double SOMVersionNumber;
+
+//! Project version string for SOM.
+FOUNDATION_EXPORT const unsigned char SOMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SOM/PublicHeader.h>
+
+
